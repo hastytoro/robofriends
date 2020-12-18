ca0cca002926c661d6b2651a9444abf3c362d54e
Luigi Lupini <luigi.lupini@gmail.com>
2020-12-18 10:34:49 +0200

    new project

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = props => {
+  <div>
+    <h1>Testing Navbar</h1>
+  </div>;
+};
+
+export default Navbar;
---
+++ src/containers/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import SearchBox from '../components/SearchBox';
+import CardList from '../components/CardList';
+import Scroll from '../components/Scroll';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
+  }
+
+  searchChangeHandler = event => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+    return !robots.length ? (
+      <h1>Loading...</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f2">Robot Friends</h1>
+        <SearchBox searchChange={this.searchChangeHandler} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
